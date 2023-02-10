8f998ca945ac06c231811269cc0df22b2f00e1c6
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-02-10 21:12:51 +0900

    230210

---
+++ SW Expert Academy/ladder1.py
@@ -0,0 +1,41 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+for _ in range(10):
+    n = int(input())
+    lst = []
+    for _ in range(100):
+        nums = [0] + list(map(int, input().split())) + [0]
+        lst.append(nums)
+    for i in range(102):
+        if lst[99][i] == 2:
+            y = i
+    x = 99
+    while x > 0:
+        lst[x][y] = "P"
+        if lst[x][y+1] == 1:
+            y = y+1
+        elif lst[x][y-1] == 1:
+            y = y-1
+        elif lst[x-1][y] == 1:
+            x = x-1
+    ans = y-1
+    print(f"#{n} {ans}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
