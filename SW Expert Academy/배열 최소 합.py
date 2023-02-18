62ca5eef5a8eafe3096d3f026201e926d66b48b9
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-02-19 00:48:20 +0900

    230219

--- SW Expert Academy/배열 최소 합.py
+++ SW Expert Academy/배열 최소 합.py
@@ -1,27 +1,27 @@
 T = int(input())
 
-def abc(level, sum):
+def abc(level, sum):    # 중복되지 않는 경로를 탐색하여 모든 합을 구해보고 최소값을 얻는 함수  
     global minSum
-    if sum > minSum: return
-    if level == n:
+    if sum > minSum: return # 최소값을 넘어가면 더 이상 탐색하지 않고 종료(탐색 횟수를 줄일 수 있음)
+    if level == n:  # 마지막 행까지 sum값을 더하면 종료. 행과 열이 중복되지 않게 lst[행번호][열번호]를 모두 더한 최종 값이 됨
         if sum < minSum:
             minSum = sum
         return
     for i in range(len(nums)):
-        if used[i] == 0:
-            used[i] = 1
-            path[level] = nums[i]
-            abc(level+1, sum + lst[level][nums[i]])
-            used[i] = 0
+        if used[i] == 0:    # 아직 사용하지 않은 열의 숫자(인덱스)면
+            used[i] = 1 # 사용표시 해준 후
+            path[level] = nums[i]  # 각 줄 인덱스로 활용     
+            abc(level+1, sum + lst[level][nums[i]]) # 매개변수로 sum값 더해가기. 더하는 값은 행과 열이 중복되지 않은 lst[행번호][열번호]
+            used[i] = 0 # 마지막 level까지 도달한 후 돌아올 때는 다시 0으로 초기화 
 
 for tc in range(1, T+1):
     n = int(input())
     lst = []
     for i in range(n):
         lst.append(list(map(int, input().split())))
-    nums = [i for i in range(n)]
-    path = [0]*n
-    used = [0]*len(nums)
+    nums = [i for i in range(n)]    # 인덱스로 사용될 숫자 목록
+    path = [0]*n    # 각 줄의 인덱스로 활용할 숫자 목록
+    used = [0]*len(nums)    # 이미 사용한 인덱스를 다시 사용하지 않기 위해 기록
     minSum = 21e8
-    abc(0, 0)
+    abc(0, 0)   # 0번 인덱스, sum=0부터 시작
     print(f"#{tc} {minSum}")
