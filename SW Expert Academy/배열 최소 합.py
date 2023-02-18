4deafdb0c8c42631e184e6b556c38a9eb85c0fe2
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-02-19 00:53:42 +0900

    230219

--- SW Expert Academy/배열 최소 합.py
+++ SW Expert Academy/배열 최소 합.py
@@ -25,3 +25,42 @@ for tc in range(1, T+1):
     minSum = 21e8
     abc(0, 0)   # 0번 인덱스, sum=0부터 시작
     print(f"#{tc} {minSum}")
+
+    
+    
+###    
+# 실패했던 코드 
+T = int(input())
+# 
+# def abc(level): # 인덱스 순열 생성하는 함수
+#     if level == n:
+#         global paths
+#         paths.append(path[:])
+#         return
+#     for i in range(len(nums)):
+#         if used[i] == 0:
+#             used[i] = 1
+#             path[level] = nums[i]
+#             abc(level+1)
+#             used[i] = 0
+# 
+# for tc in range(1, T+1):
+#     n = int(input())
+#     lst = []
+#     for i in range(n):
+#         lst.append(list(map(int, input().split())))
+#     nums = [i for i in range(n)]
+#     path = [0]*n
+#     paths = []
+#     used = [0]*len(nums)
+#     abc(0)
+# 
+#     minSum = 21e8
+#     for p in range(len(paths)): # paths에 저장된 인덱스 순열대로 하나씩 더하기
+#         sum = 0
+#         for i in range(len(lst)):
+#             sum += lst[i][paths[p][i]]
+#         if sum < minSum:    # 모든 조합의 합들 중 가장 작은 값 저장
+#             minSum = sum
+# 
+#     print(f"#{tc} {minSum}")
