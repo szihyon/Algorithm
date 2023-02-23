7d8d6d5cc9a07715e67b2a380ca0af83c9ed4e0c
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-02-23 17:24:24 +0900

    230223

--- SW Expert Academy/노드의 거리.py
+++ SW Expert Academy/노드의 거리.py
@@ -1,10 +1,9 @@
 # 풀이 1
 # DFS
-
 T = int(input())
 
 
-def dfs(S, cnt, G):
+def abc(S, cnt, G):
     global min
 
     if cnt > min: return
@@ -17,13 +16,13 @@ def dfs(S, cnt, G):
     for i in range(len(lst)):
         if lst[S][i] == 1 and used[i] == 0:
             used[i] = 1
-            dfs(i, cnt + 1, G)
+            abc(i, cnt + 1, G)
             used[i] = 0
 
 
 for tc in range(1, T + 1):
     V, E = map(int, input().split())
-    lst = [[0] * V for _ in range(V)]
+    lst = [[0] * (V) for _ in range(V)]
     for i in range(E):
         r1, r2 = map(int, input().split())
         r1 = r1 - 1
@@ -36,7 +35,7 @@ for tc in range(1, T + 1):
     used = [0] * V
     min = 21e8
     used[S] = 1
-    res = dfs(S, 0, G)
+    res = abc(S, 0, G)
 
     if min == 21e8:
         print(f"#{tc} 0")
@@ -49,37 +48,48 @@ for tc in range(1, T + 1):
 
 # 풀이2
 # BFS
+from collections import deque
 
 T = int(input())
 
+def bfs(st):
+    cnt = 0
+    q = deque()
+    q.append((st, 0))   # 첫 시작점의 위치와 이동거리(cnt)
 
-def bfs(start, end):
-    queue = [(start, 0)]
-    visited = []
-    while queue:
-        node, cnt = queue.pop(0)
-        if node == end:
+    while q:
+        now, cnt = q.popleft()  # 왼쪽부터 원소 꺼내기. 현재 위치와 이동 거리(cnt) 할당
+        if G == now:    # 도착지와 현재 위치가 같으면 cnt 리턴
             return cnt
-        if node not in visited:
-            visited.append(node)
-            for i in range(V):
-                if i not in visited and arr[node][i] == 1:
-                    queue.append((i, cnt + 1))
-    else:
-        return 0
+
+        for i in range(len(lst)):
+            if lst[now][i] == 1 and used[i] == 0:   # 경로표시가 되어있고, 아직 방문하지 않은 곳이면 
+                used[i] = 1 # 방문체크 후 
+                q.append((i, cnt+1))    # 노드번호와 이동거리(+1) 추가 
+
+    return 0
 
 
-for test_case in range(1, T + 1):
+for tc in range(1, T+1):
     V, E = map(int, input().split())
-    arr = [[0] * V for _ in range(V)]
-    for _ in range(E):
-        st, ed = map(int, input().split())
-        st -= 1
-        ed -= 1
-        arr[st][ed], arr[ed][st] = 1, 1
+    lst = [[0]*V for _ in range(V)]
+    for i in range(E):
+        r1, r2 = map(int, input().split())
+        r1 = r1-1
+        r2 = r2-1
+        lst[r1][r2] = 1
+        lst[r2][r1] = 1
+    S, G = map(int, input().split())
+    S = S-1
+    G = G-1
+    used = [0]*V
+    used[S] = 1     # 첫 시작점 경로 1로 바꿔주기
 
-    start, end = map(int, input().split())
-    start -= 1
-    end -= 1
-    result = bfs(start, end)
-    print(f'#{test_case} {result}')
+    res = bfs(S)    # bfs함수 실행
+
+    if res:
+        print(f"#{tc} {res}")
+    else:
+        print(f"#{tc} 0")
+
+
