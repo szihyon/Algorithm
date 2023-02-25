e1d5256f150ed92fdbbddb0b11485f78f2f356c3
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-02-25 15:11:13 +0900

    [Bronze I] Title: 딱지놀이, Time: 224 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.py
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -3,18 +3,18 @@ n = int(input())
 for i in range(n):
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    a_card = sorted(a[1:])
-    b_card = sorted(b[1:])
-    card = [5, 4, 3, 2, 1]
+    a_card = a[1:] # a카드목록
+    b_card = b[1:] # b카드목록
+    card = [5, 4, 3, 2, 1] # 카드 배점 큰 순서로 나열
     flag = 0
-    for i in range(len(card)):
-        if a_card.count(card[i]) > b_card.count(card[i]):
-            print("A")
+    for i in range(len(card)): # 배점 큰 카드부터 비교
+        if a_card.count(card[i]) > b_card.count(card[i]): # 카드 각자 몇 개 있는지 체크
+            print("A")  # a가 더 많으면 A출력 후 break
             flag = 1
             break
         elif a_card.count(card[i]) < b_card.count(card[i]):
-            print("B")
+            print("B") # b가 더 많으면 B출력 후 break
             flag = 1
             break
-    if flag == 0:
+    if flag == 0: # flag가 안켜졌다면 무승부
         print("D")
