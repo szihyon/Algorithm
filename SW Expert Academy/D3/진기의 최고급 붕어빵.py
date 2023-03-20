7df4ea81d1fed3b747dd14d8e151e873be4d599d
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-03-20 23:50:17 +0900

    230312

--- SW Expert Academy/D3/진기의 최고급 붕어빵.py
+++ SW Expert Academy/D3/진기의 최고급 붕어빵.py
@@ -1,3 +1,4 @@
+# 누적합 리스트 전체 -1
 T = int(input())
 
 for tc in range(1, T+1):
@@ -27,37 +28,34 @@ for tc in range(1, T+1):
         print(f"#{tc} Impossible")
 #ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
-#예전 풀이 
-import sys
-sys.stdin = open("input.txt", "r")
+# 1인 수를 발견하면 -1로 바꾸기
+# import sys
+# sys.stdin = open("input.txt", "r")
 
-# N명의 손님들. M초의 시간들이면 K개의 붕어빵.
-# 0초 이후 오는 손님들 도착시간
+# T = int(input())
 
-T = int(input())
+# def func(lst, time, n):
+#     for i in range(n):
+#         sumv = sum(lst[:time[i] + 1])
+#         if sumv != 0:
+#             for j in range(len(lst)):
+#                 if lst[j] != 0:
+#                     lst[j] -= 1
+#                     break
+#         else:
+#             return print(f"#{tc} Impossible")
+#     return print(f"#{tc} Possible")
 
-def func(lst, time, n):
-    for i in range(n):
-        sumv = sum(lst[:time[i] + 1])
-        if sumv != 0:
-            for j in range(len(lst)):
-                if lst[j] != 0:
-                    lst[j] -= 1
-                    break
-        else:
-            return print(f"#{tc} Impossible")
-    return print(f"#{tc} Possible")
-
-for tc in range(1, T+1):
-    n, m, k = map(int, input().split())
-    temp = m
-    time = list(map(int, input().split()))
-    time.sort()
-    lst = [0] * 11112
-    while temp < len(lst):
-        lst[temp] = k
-        temp += m
-    func(lst, time, n)
+# for tc in range(1, T+1):
+#     n, m, k = map(int, input().split())
+#     temp = m
+#     time = list(map(int, input().split()))
+#     time.sort()
+#     lst = [0] * 11112
+#     while temp < len(lst):
+#         lst[temp] = k
+#         temp += m
+#     func(lst, time, n)
 
 
 
