90cd65e35616433c90dbea2d4ad226fa55c8ddf4
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-04-01 23:10:54 +0900

    [Silver III] Title: N과 M (5), Time: 192 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+def abc(level):
+    if level == m:
+        print(*path)
+        return
+
+    for j in range(len(nums)):
+        if used[j] == 1: continue
+        used[j] = 1
+        path[level] = nums[j]
+        abc(level+1)
+        used[j] = 0
+
+path = [0]*m
+used = [0]*n
+abc(0)
