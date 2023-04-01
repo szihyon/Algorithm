afeb17169365c08418778e6299e715fe02fe9db5
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-04-01 22:13:43 +0900

    [Silver III] Title: N과 M (1), Time: 228 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+nums = [i for i in range(1, n+1)]
+
+def abc(level):
+    if level == m:
+        for i in range(len(path)):
+            print(path[i], end=' ')
+        print()
+        return
+    for j in range(len(nums)):
+        if used[j] == 1: continue
+        used[j] = 1
+        path[level] = nums[j]
+        abc(level+1)
+        used[j] = 0
+
+path = [0]*m
+used = [0]*n
+abc(0)
