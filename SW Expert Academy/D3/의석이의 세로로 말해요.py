39b0bf707b45e0b485bacfd6286daeaa026a7e5a
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-04-03 00:29:36 +0900

    230403

--- SW Expert Academy/D3/의석이의 세로로 말해요.py
+++ SW Expert Academy/D3/의석이의 세로로 말해요.py
@@ -12,3 +12,15 @@ for tc in range(1, T+1):
             if lst[j][i] != "#":
                 print(lst[j][i], end='')
     print()
+
+    
+# 방법 2
+# T = int(input())
+# for tc in range(1, T+1):
+#     arr = [input() for _ in range(5)]
+#     ans = ''
+#     for j in range(15):
+#         for i in range(5):
+#             if j < len(arr[i]):
+#                 ans += arr[i][j]
+#     print(f"#{tc} {ans}")
