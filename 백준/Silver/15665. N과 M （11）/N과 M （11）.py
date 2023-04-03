58ecd983d192df254f96ef3af0a3869e6a15eaf2
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-04-03 13:45:37 +0900

    [Silver II] Title: N과 M (11), Time: 1316 ms, Memory: 57196 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+def abc(level):
+    if level == m:
+        num = ' '.join(map(str, path))
+        if num not in dic:
+            dic[num] = 1
+            print(num)
+        return
+
+    for j in range(len(nums)):
+        path[level] = nums[j]
+        abc(level+1)
+
+path = [0]*m
+dic = dict()
+abc(0)
