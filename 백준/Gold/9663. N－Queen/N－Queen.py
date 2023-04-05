26cb24e2832125796e354cf5fdbb0c175f6daf08
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-04-06 00:57:18 +0900

    [Gold IV] Title: N-Queen, Time: 25904 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -8,23 +8,29 @@ cnt = 0
 
 def func(level):
     global cnt
+    # 마지막 행까지 도달하면 카운트
     if level == n:
         cnt += 1
         return
+        
     for i in range(n):
     	# 열 중복체크. 1이면 continue
+        # dat[열]
         if dat[i] == 1:
             continue
-        # 우상향 대각선 중복체크
+        # 우상향 대각선 중복체크, rup[행+열]
         if rup[level + i] == 1: continue
-        # 우하향 대각선 중복체크
+        # 우하향 대각선 중복체크, rdown[(행-열)+(n-1)]
         if rdown[(level - i) + (n-1)] == 1: continue
-        # 모두 중복이 아니라면 1체크 후,
+        
+        # 모두 중복이 아니라면 1체크 후, 
         rup[level + i] = 1
         rdown[(level - i) + (n-1)] = 1
         dat[i] = 1
+        
         # 다음 행 함수 호출
         func(level + 1)
+        
         # return하면 다시 0으로 갱신
         dat[i] = 0
         rup[level + i] = 0
