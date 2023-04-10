f21d14d4c06626190514525e1f58cdbc247814a7
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-04-10 10:44:00 +0900

    [Gold IV] Title: 알파벳, Time: 1208 ms, Memory: 51736 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,3 +1,5 @@
+# 방법 1 
+# dfs코드 pypy로 돌리기
 r, c = map(int, input().split())
 lst = [list(input()) for _ in range(r)]
 
@@ -25,3 +27,35 @@ path = []
 path.append(lst[0][0])
 res = bfs(0, 0, 1, lst[0][0])
 print(res)
+
+
+
+# 방법 2
+# set이용해서 bfs처럼 풀기
+# r, c = map(int, input().split())
+# lst = [list(input()) for _ in range(r)]
+# 
+# def bfs(stY, stX, cnt, path):
+#     directY = [-1, 0, 0, 1]
+#     directX = [0, -1, 1, 0]
+#     q = set()
+#     q.add((stY, stX, cnt, path))
+#     M_cnt = 0
+# 
+#     while q:
+#         nowY, nowX, cnt, path = q.pop()
+#         if cnt > M_cnt:
+#             M_cnt = cnt
+#         for i in range(4):
+#             dy = directY[i] + nowY
+#             dx = directX[i] + nowX
+#             if dy < 0 or dx < 0 or dy > r-1 or dx > c-1:continue
+#             if lst[dy][dx] in path: continue
+#             q.add((dy, dx, cnt+1, path+lst[dy][dx]))
+# 
+#     return M_cnt
+# 
+# path = []
+# path.append(lst[0][0])
+# res = bfs(0, 0, 1, lst[0][0])
+# print(res)
