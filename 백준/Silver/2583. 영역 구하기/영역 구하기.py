daa9b81a98de5125b34c5e6bb16c53435775e040
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-04-13 00:24:18 +0900

    [Silver I] Title: 영역 구하기, Time: 72 ms, Memory: 34192 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+def bfs(stY, stX):
+    q = deque()
+    q.append((stY, stX))
+    directY = [-1, 0, 0, 1]
+    directX = [0, -1, 1, 0]
+    cnt2 = 0
+    lst[stY][stX] = 1
+    cnt2 += 1
+
+    while q:
+        nowY, nowX = q.popleft()
+
+        for i in range(4):
+            dy = directY[i] + nowY
+            dx = directX[i] + nowX
+            if dy < 0 or dx < 0 or dy > m-1 or dx > n-1:continue
+            if lst[dy][dx] == 1:continue
+            lst[dy][dx] = 1
+            cnt2 += 1
+            q.append((dy, dx))
+
+    return cnt2
+
+m, n, k = map(int, input().split())
+lst = [[0]*n for _ in range(m)]
+
+cnt = 0
+ans = []
+for _ in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            lst[i][j] = 1
+
+for i in range(m):
+    for j in range(n):
+        if lst[i][j] == 0:
+            cnt += 1
+            area = bfs(i, j)
+            ans.append(area)
+
+print(cnt)
+ans.sort()
+print(*ans)
