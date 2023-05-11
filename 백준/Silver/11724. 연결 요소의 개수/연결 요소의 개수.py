aca05324a3a9083e7a033e34c3af34f2ca47ab6c
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-05-11 10:30:16 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 588 ms, Memory: 124648 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,33 +1,27 @@
-from collections import deque
-
-import sys
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
-lst = [[0] * n for _ in range(n)]
+lst = [[0]*n for _ in range(n)]
 for _ in range(m):
     p1, p2 = map(int, input().split())
-    lst[p1 - 1][p2 - 1] = 1
-    lst[p2 - 1][p1 - 1] = 1
+    lst[p1-1][p2-1] = 1
+    lst[p2-1][p1-1] = 1
 
 visit = [0]*n
 
-def bfs(st):
-    q = deque()
-    q.append(st)
-
-    while q:
-        now = q.popleft()
-        for i in range(n):
-            if lst[now][i] == 1 and visit[i] == 0:
+def dfs(now):
+    for i in range(n):
+        if lst[now][i] == 1:
+            if visit[i] == 0:
                 visit[i] = 1
-                q.append(i)
+                dfs(i)
 
 cnt = 0
-for i in range(n):
-    if visit[i] == 0:
-        visit[i] = 1
-        cnt += 1
-        bfs(i)
+
+for j in range(n):
+    # for j in range(n):
+    #     if lst[i][j] == 1:
+            if visit[j] == 0:
+                visit[j] = 1
+                cnt += 1
+                dfs(j)
 
 print(cnt)
