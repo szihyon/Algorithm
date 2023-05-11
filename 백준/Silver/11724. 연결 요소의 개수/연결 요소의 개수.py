385cc353656c73599164b1ba673f93961839abef
ji-hyon <120673992+ji-hyon@users.noreply.github.com>
2023-05-11 13:43:55 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 592 ms, Memory: 38416 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,28 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
-lst = [[0]*n for _ in range(n)]
+lst = [[0] * n for _ in range(n)]
 for _ in range(m):
     p1, p2 = map(int, input().split())
-    lst[p1-1][p2-1] = 1
-    lst[p2-1][p1-1] = 1
+    lst[p1 - 1][p2 - 1] = 1
+    lst[p2 - 1][p1 - 1] = 1
 
-visit = [0]*n
+visit = [0] * n
 
 def dfs(now):
+    visit[now] = 1
+
     for i in range(n):
-        if lst[now][i] == 1:
-            if visit[i] == 0:
-                visit[i] = 1
-                dfs(i)
+        if lst[now][i] == 1 and visit[i] == 0:
+            dfs(i)
 
 cnt = 0
 
-for j in range(n):
-    # for j in range(n):
-    #     if lst[i][j] == 1:
-            if visit[j] == 0:
-                visit[j] = 1
-                cnt += 1
-                dfs(j)
+for i in range(n):
+    if visit[i] == 0:
+        cnt += 1
+        dfs(i)
 
 print(cnt)
