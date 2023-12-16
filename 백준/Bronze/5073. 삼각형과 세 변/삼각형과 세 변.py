0430bf06589e5e419298d2367b302436d86ef251
JiHyon Seo <gyg03134@gmail.com>
2023-12-17 01:19:38 +0900

    Update 삼각형과 세 변.py

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -6,11 +6,10 @@ while(True):
 
     if a==b==c:
         print("Equilateral")
-    elif a>=b+c or b>=a+c or c>=a+b:
+    elif a>=b+c or b>=a+c or c>=a+b:  #  a + b + c - max_ <= max_
+                                      #  2 * max(a, b, c) >= a + b + c
         print("Invalid")
     elif a==b or b==c or a==c:
         print("Isosceles")
     else:
         print("Scalene")
-
-
