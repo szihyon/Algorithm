33bb913e637c187f692c3dfc11eb737e5ad871b7
JiHyon Seo <gyg03134@gmail.com>
2024-01-05 14:10:19 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,12 +1,11 @@
 def solution(players, callings):
     answer = []
     players_dict = {players[i]:i for i in range(len(players))}
-    
     for call in callings:
         back_index = players_dict[call]
         front_index = back_index - 1
-        players[front_index], players[back_index] = players[back_index], players[front_index]
-        players_dict[call]=front_index
-        players_dict[players[back_index]]=back_index
+        players_dict[call] = front_index
+        players_dict[players[front_index]] = back_index
+        players[back_index], players[front_index] = players[front_index], players[back_index]
     answer = players
     return answer
