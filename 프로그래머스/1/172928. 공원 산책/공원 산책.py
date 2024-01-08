2db42f6d7a6250e20c468d1706ace1d780578737
JiHyon Seo <gyg03134@gmail.com>
2024-01-08 15:23:42 +0900

    [level 1] Title: 공원 산책, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,5 +1,7 @@
 def solution(park, routes):
     answer = []
+
+    # 시작 지점 찾기
     flag = 0
     h = len(park)
     w = len(park[0])
@@ -11,45 +13,27 @@ def solution(park, routes):
                 nowX = j
                 flag = 1
                 break
+    
+    # 방향 정보
+    directionY = {"E":0, "W":0, "N":-1, "S":1}
+    directionX = {"E":1, "W":-1, "N":0, "S":0}
+
+    # 이동
     for i in range(len(routes)):
         flag = 0
         direction, distance = routes[i].split()
         distance = int(distance)
-        if direction == "E":
-            if nowX + distance < 0 or nowX + distance > w-1: continue
-            for i in range(1, distance + 1):
-                if park[nowY][nowX + i] == "X":
-                    flag = 1
-                    break
-            if flag == 1:
-                continue
-            nowX += distance
-        elif direction == "W":
-            if nowX - distance < 0 or nowX - distance > w-1: continue
-            for i in range(1, distance + 1):
-                if park[nowY][nowX - i] == "X":
-                    flag = 1
-                    break
-            if flag == 1:
-                continue
-            nowX -= distance
-        elif direction == "N":
-            if nowY - distance < 0 or nowY - distance > h-1: continue
-            for i in range(1, distance + 1):
-                if park[nowY - i][nowX] == "X":
-                    flag = 1
-                    break
-            if flag == 1:
-                continue
-            nowY -= distance
-        elif direction == "S":
-            if nowY + distance < 0 or nowY + distance > h-1: continue
-            for i in range(1, distance + 1):
-                if park[nowY + i][nowX] == "X":
-                    flag = 1
-                    break
-            if flag == 1:
-                continue
-            nowY += distance
+        for i in range(1, distance+1):
+            dy = nowY + directionY[direction] * i
+            dx = nowX + directionX[direction] * i
+            if dy < 0 or dy > h-1 or dx < 0 or dx > w-1: 
+                flag = 1
+                break
+            if park[dy][dx] == "X": 
+                flag = 1
+                break
+        if flag == 1: continue
+        nowY = dy
+        nowX = dx
     answer = [nowY, nowX]
     return answer
