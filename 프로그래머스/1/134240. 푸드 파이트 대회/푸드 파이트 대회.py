a22f1243759f668924b7bd87c242089a24f94393
JiHyon Seo <gyg03134@gmail.com>
2024-01-20 19:05:42 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,8 +1,5 @@
 def solution(food):
     answer = ''
-    for i in range(1, len(food)):
-        if food[i] % 2 == 1:
-            food[i] -= 1
     n = len(food)
     for i in range(1, n):
         answer += str(i)*(food[i]//2)
