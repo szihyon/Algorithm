0e6c376e277911cc7fa4581bee29e9ead08134c4
szihyon <gyg03134@gmail.com>
2024-02-07 21:56:45 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1256 ms, Memory: 718348 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
+
+n, m, r = map(int, input().split())
+lst = [[] for _ in range(n+1)]
+res = [0]*(n+1)
+
+for i in range(m):
+    n1, n2 = map(int, input().split())
+    lst[n1].append(n2)
+    lst[n2].append(n1)
+
+for i in range(1, len(lst)):
+    lst[i].sort()
+
+cnt = 1
+def dfs(node):
+    global cnt
+    res[node] = cnt
+
+    for n in lst[node]:
+        if res[n]: continue
+        cnt += 1
+        dfs(n)
+
+res[r] = 1
+dfs(r)
+
+for i in res[1:]:
+    print(i)
