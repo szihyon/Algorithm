329790eca75a9be584c6e1c4b220cf61200e4f6c
szihyon <gyg03134@gmail.com>
2024-02-10 20:47:18 +0900

    [Silver I] Title: 행운의 문자열, Time: 2432 ms, Memory: 113808 KB -BaekjoonHub

--- 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
+++ 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
@@ -1,28 +1,33 @@
 s = list(input())
 n = len(s)
-cnt = dict()
+s_dict = dict()
 answer = 0
 
 # 주어진 알파벳 딕셔너리 만들기
-for i in s:
-    if i in cnt:
-        cnt[i] += 1
+for a in s:
+    if a in s_dict:
+        s_dict[a] += 1
     else:
-        cnt[i] = 1
+        s_dict[a] = 1
 
-
-def dfs(level, path):
+# dfs 함수
+def dfs(level, lucky_str):
     global answer
+		# 주어진 알파벳들을 전부 사용하면 정답+1
     if level == n:
         answer += 1
         return
-
-    for i in cnt:
-        if cnt[i] == 0: continue
-        if level > 0 and i == path[-1]: continue
-        cnt[i] -= 1
-        dfs(level+1, path+i)
-        cnt[i] += 1
+		
+		# 알파벳을 딕셔너리에서 하나씩 꺼내기 
+    for a in s_dict:
+				# 알파벳이 남아있지 않으면 continue
+        if s_dict[a] == 0: continue
+				# 직전에 사용한 문자와 같으면 continue
+        if level > 0 and a == lucky_str[-1]: continue
+				# 조건 통과후 알파벳 사용
+        s_dict[a] -= 1
+        dfs(level+1, lucky_str+a)
+        s_dict[a] += 1 # return 후 돌아올 때 다시 +1
 
 dfs(0, '')
 print(answer)
