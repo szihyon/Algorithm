c1de597d8e82164d9bd906ccb163c4a932e6815d
szihyon <gyg03134@gmail.com>
2024-02-19 01:55:13 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 464 ms, Memory: 132504 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+N, M, R = map(int, input().split())
+lst = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    lst[u].append(v)
+    lst[v].append(u)
+
+for i in range(N+1):
+    lst[i].sort(reverse=True)
+
+def bfs(start):
+    global visit
+    q = deque()
+    q.append(start)
+    visit = [0]*(N+1)
+    cnt = 1
+    visit[start] = cnt
+
+    while q:
+        now = q.popleft()
+        for node in lst[now]:
+            if visit[node] >= 1: continue
+            cnt += 1
+            visit[node] = cnt
+            q.append(node)
+    return
+
+bfs(R)
+for i in range(1, len(visit)):
+    print(visit[i])
