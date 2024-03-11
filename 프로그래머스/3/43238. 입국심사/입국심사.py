1f7225c3a2f39eb5de697a4ca62ef4e68f09fd91
szihyon <gyg03134@gmail.com>
2024-03-12 00:53:59 +0900

    [level 3] Title: 입국심사, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,15 +1,15 @@
 def solution(n, times):
     min_time = 1
-    max_time = max(times) * n # 모든 사람이 가장 오래 걸리는 심사대를 이용할 때의 시간
+    max_time = max(times) * (n-1) # 모든 사람이 가장 오래 걸리는 심사대를 이용할 때의 시간
 
-    answer = max_time
     while min_time <= max_time:
         mid_time = (min_time + max_time) // 2
         total = sum(mid_time // time for time in times)
 
         if total >= n: # 모든 사람을 심사할 수 있는 경우, 시간을 줄여본다
-            answer = min(answer, mid_time)
             max_time = mid_time - 1
         else: # 모든 사람을 심사할 수 없는 경우, 시간을 늘린다
             min_time = mid_time + 1
+            
+    answer = min_time
     return answer
