077f2db2ee2f95e1a9af205755c90d0765272106
szihyon <gyg03134@gmail.com>
2024-05-18 20:29:48 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,20 +1,49 @@
 import heapq
 def solution(operations):
     answer = []
+    min_Q = []
+    max_Q = []
+    nums = dict()
+    
     for operation in operations:
         op, num = operation.split()
         num = int(num)
+        # 숫자 삽입
         if op == 'I':
-            heapq.heappush(answer, num)
-        else:
-            if num == 1:
-                if answer:
-                    answer.pop(-1)
+            if num not in nums:
+                nums[num] = 1
             else:
-                if answer:
-                    answer.pop(0)
-    if len(answer) == 0:
-        answer = [0, 0]
+                nums[num] += 1
+            heapq.heappush(min_Q, num)
+            heapq.heappush(max_Q, -num)
+        # 숫자 삭제
+        else: 
+            if num == 1: # 최댓값 삭제
+                while max_Q:
+                    maxV = -1 * heapq.heappop(max_Q)
+                    if maxV in nums and nums[maxV] > 0:
+                        nums[maxV] -= 1
+                        if nums[maxV] == 0:
+                            del nums[maxV]
+                        break
+                
+            else: # 최솟값 삭제
+                while min_Q:
+                    minV = heapq.heappop(min_Q)
+                    if minV in nums and nums[minV] > 0:
+                        nums[minV] -= 1
+                        if nums[minV] == 0:
+                            del nums[minV]
+                        break
+
+    while max_Q and (-max_Q[0] not in nums):
+        heapq.heappop(max_Q)
+    while min_Q and (min_Q[0] not in nums):
+        heapq.heappop(min_Q)
+    
+    if max_Q and min_Q:
+        answer = [-max_Q[0], min_Q[0]]
     else:
-        answer = [max(answer), min(answer)]
+        answer = [0, 0]
+        
     return answer
