3c32755c8b8603de684879fe0d819e89083cb7ad
szihyon <gyg03134@gmail.com>
2024-06-09 17:28:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,27 @@
 from collections import deque
 
 def solution(maps):
-    answer = 0
     n = len(maps)
     m = len(maps[0])
-    
-    directY = [0, 0, 1, -1]
-    directX = [1, -1, 0, 0]
-    
+
     q = deque()
     q.append([0, 0, 1])
+    visit = [[0]*m for _ in range(n)]
+    visit[0][0] = 1
+    
+    directY = [-1, 1, 0, 0]
+    directX = [0, 0, -1, 1]
     
     while q:
-        nowY, nowX, move = q.popleft()
+        nowY, nowX, cnt = q.popleft()
         if nowY == n-1 and nowX == m-1:
-            return move
+            return cnt
         for i in range(4):
-            dy = directY[i] + nowY
-            dx = directX[i] + nowX
-            if dy < 0 or dx < 0 or dy > n-1 or dx > m-1: continue
-            if maps[dy][dx] == 0: continue
-            maps[dy][dx] = 0
-            q.append([dy, dx, move+1])
-    
+            dy = nowY + directY[i]
+            dx = nowX + directX[i]
+            if dy > n-1 or dx > m-1 or dy < 0 or dx < 0:continue
+            if maps[dy][dx] == 0 : continue 
+            if visit[dy][dx] == 1 : continue
+            visit[dy][dx] = 1
+            q.append((dy, dx, cnt+1))
     return -1
