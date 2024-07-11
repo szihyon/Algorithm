f3b5712e77f481bde9f111887fa9db137332ba0c
szihyon <gyg03134@gmail.com>
2024-07-11 20:34:52 +0900

    [Bronze IV] Title: 빵, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -9,8 +9,8 @@ for _ in range(N):
     elif A == B:    # 도착할 때 빵이 들어오는 경우
         time = A    # 이동시간
     else:   # 도착 전에 빵이 들어오는 경우
-        time = A    # 이동시간
         cnt += 1    # 빵을 구할 수 없는 경우 카운트
+        continue
 
     if time < answer:   
         answer = time   # 가장 빨리 구할 수 있는 시간 갱신 
