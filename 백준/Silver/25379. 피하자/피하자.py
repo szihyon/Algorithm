6c7151ef9017c154084cc4cfa130ef66b0f99d14
JiHyon Seo <gyg03134@gmail.com>
2024-07-27 20:27:34 +0900

    [Silver IV] Title: 피하자, Time: 948 ms, Memory: 149896 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/25379. 피하자/피하자.py
+++ 백준/Silver/25379. 피하자/피하자.py
@@ -1,28 +1,36 @@
 N = int(input())
-nums = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+cnt_odd_left = 0
 cnt = 0
+for a in arr:
+    if a % 2 == 1:
+        cnt += 1
+    else:
+        cnt_odd_left += cnt 
 
-# 홀수와 짝수가 인접한 경우 세기
-def count(nums):
-    global cnt
-    cnt = 0
-    for i in range(N-1):
-        if nums[i] % 2 != nums[i+1] % 2:
-            cnt += 1
-            
-count(nums) # 처음 배열에 대해 카운트
+cnt_odd_right = 0
+cnt = 0
+for a in arr[::-1]:
+    if a % 2 == 1:
+        cnt += 1
+    else:
+        cnt_odd_right += cnt 
 
-# 인접한 경우가 2개 이상인 경우,
-# 홀수와 짝수가 인접한 경우가 한 번이 될 때까지 원소 교환
-answer = 0
-while cnt > 1:
-    for i in range(N - 1):
-        if nums[i] % 2 != nums[i + 1] % 2:
-            nums[i], nums[i + 1] = nums[i + 1], nums[i]
-            answer += 1 # 교환한 횟수 + 1
-            count(nums) # 교환 후 홀수와 짝수가 인접한 경우 세기
-            if cnt == 1: # 인접한 경우가 한 번이 되면 반복문 종료
-                break
+cnt_even_left = 0
+cnt = 0
+for a in arr:
+    if a % 2 == 0:
+        cnt += 1
+    else:
+        cnt_even_left += cnt 
 
-# 정답 출력
-print(answer)
+cnt_even_right = 0
+cnt = 0
+for a in arr[::-1]:
+    if a % 2 == 0:
+        cnt += 1
+    else:
+        cnt_even_right += cnt 
+
+print(min(cnt_odd_left, cnt_odd_right, cnt_even_left, cnt_even_right))
