b4ec1c84b5d302fd5d5c7f119d3b43f902a87a5e
JiHyon Seo <gyg03134@gmail.com>
2024-07-27 20:29:29 +0900

    [Silver IV] Title: 피하자, Time: 476 ms, Memory: 258404 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/25379. 피하자/피하자.py
+++ 백준/Silver/25379. 피하자/피하자.py
@@ -9,14 +9,6 @@ for a in arr:
     else:
         cnt_odd_left += cnt 
 
-cnt_odd_right = 0
-cnt = 0
-for a in arr[::-1]:
-    if a % 2 == 1:
-        cnt += 1
-    else:
-        cnt_odd_right += cnt 
-
 cnt_even_left = 0
 cnt = 0
 for a in arr:
@@ -25,12 +17,5 @@ for a in arr:
     else:
         cnt_even_left += cnt 
 
-cnt_even_right = 0
-cnt = 0
-for a in arr[::-1]:
-    if a % 2 == 0:
-        cnt += 1
-    else:
-        cnt_even_right += cnt 
 
-print(min(cnt_odd_left, cnt_odd_right, cnt_even_left, cnt_even_right))
+print(min(cnt_odd_left, cnt_even_left))
