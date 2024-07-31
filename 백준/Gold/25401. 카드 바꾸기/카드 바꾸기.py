45de3e43847db24a692ae251bd9bcedf2acc79d8
JiHyon Seo <gyg03134@gmail.com>
2024-07-31 16:16:08 +0900

    [Gold V] Title: 카드 바꾸기, Time: 520 ms, Memory: 110736 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.py
+++ 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.py
@@ -1,28 +1,27 @@
 N = int(input())
 nums = list(map(int, input().split()))
 
-num_dict = {}
+# 같은 숫자로 만들기
+num_count = {}
 for num in nums:
-    if num in num_dict:
-        num_dict[num] += 1
+    if num in num_count:
+        num_count[num] += 1
     else:
-        num_dict[num] = 1
+        num_count[num] = 1
+same_number_changes = N - max(num_count.values())
 
-# 모든 수를 같은 수로 만들 때의 최소 변경 횟수
-same_number_changes = N - max(num_dict.values())
-
-# 일정한 차이로 증가/감소하는 수열을 만들 때의 최소 변경 횟수
-min_diff_changes = float('inf')
+# 일정한 차이로 만드는 경우
+min_changes = N - 1  # 모든 숫자가 다르다고 가정하고 시작
 for i in range(N):
     for j in range(i + 1, N):
-        d = nums[j] - nums[i]
-        count = 0
-        for k in range(N):
-            expected_value = nums[i] + d * (k - i)
-            if nums[k] != expected_value:
-                count += 1
-        min_diff_changes = min(min_diff_changes, count)
+        if (nums[j] - nums[i]) % (j - i) == 0:
+            d = (nums[j] - nums[i]) // (j - i)
+            count = 0
+            for k in range(N):
+                if nums[k] != nums[i] + d * (k - i):
+                    count += 1
+            min_changes = min(min_changes, count)
 
-# 정답은 두 경우 중 최소값
-answer = min(same_number_changes, min_diff_changes)
+# 최종 답
+answer = min(same_number_changes, min_changes)
 print(answer)
