ba05d57da56d001126430090efd568512c4a1533
JiHyon Seo <gyg03134@gmail.com>
2025-06-04 00:51:11 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,17 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-nums = list((input().split()))
-num1, num2, num3 = map(int, nums)
+a, b, c = map(int, input().split())
 
-if num1 == num2 == num3:
-    print(10000 + num1 * 1000)
-elif num1 == num2 and num1 != num3:
-    print(1000 + num1 * 100)
-elif num2 == num3 and num1 != num2:
-    print(1000 + num2 * 100)
-elif num1 == num3 and num1 != num2:
-    print(1000 + num1 * 100)
+if a == b == c:
+    print(10000 + a * 1000)
+elif a == b:
+    print(1000 + a * 100)
+elif a == c:
+    print(1000 + a * 100)
+elif b == c:
+    print(1000 + b * 100)
 else:
-    nums.sort(reverse=True)
-    print(int(nums[0]) * 100)
+    print(100 * max(a, b, c))
