6c4a13b2ba57c4e92b5166c650f712f6ccc5f73a
JiHyon Seo <gyg03134@gmail.com>
2025-06-10 13:03:03 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,19 +1,6 @@
-S = ['']*5
+words = [input() for i in range(5)]
 
-for i in range(5):
-    S[i] = list(input())
-
-maxLen = 0
-for s in S:
-    if len(s) > maxLen:
-        maxLen = len(s)
-
-for s in S:
-    if len(s) != maxLen:
-        for _ in range(maxLen-len(s)):
-            s.append('')
-# print(S)
-
-for i in range(maxLen):
-    for j in range(5):
-        print(S[j][i], end='')
+for j in range(15):
+    for i in range(5):
+        if j < len(words[i]):
+            print(words[i][j], end='')
