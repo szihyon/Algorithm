093829312601c587d0814acca05e762c300f5efd
JiHyon Seo <gyg03134@gmail.com>
2025-09-05 11:16:14 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.04 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,43 +1,56 @@
 def solution(video_len, pos, op_start, op_end, commands):
-    answer = ''
+    """
+    - 오프닝 구간에 있으면 자동으로 오프닝 끝으로 이동
+    - next: 10초 앞으로, prev: 10초 뒤로
+    """
     
-    #초 변환
-    mi, sec = map(int, video_len.split(":"))
-    video_len = mi*60 + sec
-    mi, sec = map(int, pos.split(":"))
-    pos = mi*60 + sec
-    mi, sec = map(int, op_start.split(":"))
-    op_start = mi*60 + sec
-    mi, sec = map(int, op_end.split(":"))
-    op_end = mi*60 + sec
+    def time_to_seconds(time_str):
+        """mm:ss 형식을 초로 변환"""
+        minutes, seconds = map(int, time_str.split(":"))
+        return minutes * 60 + seconds
     
+    def seconds_to_time(seconds):
+        """초를 mm:ss 형식으로 변환"""
+        minutes = seconds // 60
+        secs = seconds % 60
+        return f"{minutes:02d}:{secs:02d}"
     
-
+    def skip_opening(position, op_start, op_end):
+        """오프닝 구간에 있으면 오프닝 끝으로 이동"""
+        return op_end if op_start <= position < op_end else position
     
-    while commands:
-        if op_start <= pos < op_end: #이동 전 오프닝 구간인 경우
-            pos = op_end
-        if commands[0] == "next":   #뒤로 가기
-            if pos + 10 > video_len:    #비디오 끝보다 뒤인 경우
-                pos = video_len #비디오 끝으로 이동
-            elif op_start <= pos + 10 < op_end: #오프닝 구간인 경우
-                pos = op_end    #오프닝 끝으로 이동
-            else:   #그 외
-                pos = pos + 10  #10초 뒤로 이동
-        elif commands[0] == "prev": #앞으로 가기
-            if pos - 10 < 0:    #비디오 시작보다 앞인 경우
-                pos = 0 #시작 부분으로 이동
-            elif op_start <= pos - 10 < op_end: #오프닝 구간인 경우
-                pos = op_end
-            else:
-                pos = pos - 10
-        if op_start <= pos < op_end: #이동 후 오프닝 구간인 경우
-            pos = op_end
-        del commands[0]
+    # 시간을 초로 변환
+    video_len_sec = time_to_seconds(video_len)
+    current_pos = time_to_seconds(pos)
+    op_start_sec = time_to_seconds(op_start)
+    op_end_sec = time_to_seconds(op_end)
     
-    mi = pos // 60
-    sec = pos % 60
+    # 시작 위치가 오프닝 구간이면 오프닝 끝으로 이동
+    current_pos = skip_opening(current_pos, op_start_sec, op_end_sec)
     
-    answer = f"{mi:02}:{sec:02}"
+    # 명령어 처리
+    for command in commands:
+        if command == "next":
+            # 10초 앞으로 이동
+            new_pos = current_pos + 10
             
-    return answer
+            # 동영상 끝을 넘으면 동영상 끝으로
+            if new_pos >= video_len_sec:
+                current_pos = video_len_sec
+            else:
+                current_pos = new_pos
+                
+        elif command == "prev":
+            # 10초 뒤로 이동
+            new_pos = current_pos - 10
+            
+            # 동영상 시작보다 앞이면 시작으로
+            if new_pos < 0:
+                current_pos = 0
+            else:
+                current_pos = new_pos
+        
+        # 명령어 처리 후 오프닝 구간 체크
+        current_pos = skip_opening(current_pos, op_start_sec, op_end_sec)
+    
+    return seconds_to_time(current_pos)
