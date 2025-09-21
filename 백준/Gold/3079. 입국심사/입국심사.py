319a67d2b9dd7069c6f075ddee23fbe5ca16b8c2
JiHyon Seo <gyg03134@gmail.com>
2025-09-21 13:11:04 +0900

    [Gold V] Title: 입국심사, Time: 3492 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+t_lst = []
+for _ in range(N):
+    t_lst.append(int(input()))
+
+answer = 0
+left = 0
+right = max(t_lst) * M  #가장 오래걸리는 심사대에만 줄서는 경우 시간
+
+#이분탐색
+while left <= right:
+    mid = (left+right)//2
+    people = 0
+    
+    for t in t_lst:
+        people += mid//t
+    
+    if people < M:    #상근이와 친구들 수 보다 적으면
+        left = mid+1    #시간 더 늘리기
+        answer = mid+1
+    else:
+        right = mid-1
+
+print(answer)
